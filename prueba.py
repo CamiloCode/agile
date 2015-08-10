278168f96b589f8dac2c60c2ca5a547e0ab7d8f0
NicolasBonet <n.bonet2476@uniandes.edu.co>
2015-08-09 21:33:08 -0500

    Primer commit de aplicación galeria en Python, es una aproximación para generar archivo html con las imágenes cargadas en el folder

---
+++ prueba.py
@@ -0,0 +1,36 @@
+import os
+
+index=os.listdir('./Images')
+
+x=len(index)
+
+for fname in index:
+    while x>0:
+        x=x-1
+        index[x] = '<a href="./' + index[x].replace("jpg", "html") + '">' + '<img src="./Thumbs/' + index[x] + '" />' + '</a>'
+
+listString='\n'.join(index)
+
+title=os.getcwd()
+title=title.split("/")
+title=title.pop()
+
+file = open("index.html", 'w')
+
+file.write('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"' + '\n')
+file.write('    "http://www.w3.org/TR/html4/loose.dtd">' + '\n')
+file.write('<html>' + '\n')
+file.write('<title>' + title + '</title>' + '\n')
+file.write('<head>' + '\n')
+file.write('<style>' + '\n')
+file.write('body {padding:10px;background-color:black;margin-left:15%;margin-right:15%;font-family:"Lucida Grande",Verdana,Arial,Sans-Serif;color: white;}' + '\n')
+file.write('img {border-style:solid;border-width:5px;border-color:white;}' + '\n')
+file.write('</style>' + '\n')
+file.write('</head>' + '\n')
+file.write('<body>' + '\n')
+file.write('<h1>' + title + '</h1>' + '\n')
+file.write(listString + '\n')
+file.write('</body>' + '\n')
+file.write('</html>')
+
+file.close()
