4c69953b3ff0107c8b3e4647f41a5bdaeec38dd4
Omar Contreras <omarcontreras@Omars-MacBook-Pro.local>
2015-08-10 23:40:54 -0500

    clean up

--- gallery/photos/settings.py
+++
@@ -1,8 +0,0 @@
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Nuestra app de Photos
-MEDIA_ROOT = '/Users/mac/Dropbox/Universidad/2015-2/Procesos Agiles/django/gallery/photos/media'
-MEDIA_URL = 'http://127.0.0.1:8000/media/'
