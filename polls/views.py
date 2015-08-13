2490c98e1bf03648ccd4d8ca340d7f6555660d38
Omar Contreras <omarcontreras@Omars-MacBook-Pro.local>
2015-08-13 03:01:12 -0500

    test

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-	return HttpResponse("hello, world. you're at the pool index.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ', '.join([p.question_text for p in latest_question_list])
+	return HttpResponse(output)
